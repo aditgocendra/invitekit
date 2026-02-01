4cb9a2a403f74747080eb16209d1e0cd75cb60fb
aditgocendra <gocendra123@gmail.com>
2026-02-01 22:03:54 +0700

    refactory template

--- templates/free/wedding/minimal/component/session-section.tsx
+++ templates/free/wedding/minimal/component/session-section.tsx
@@ -1,12 +1,24 @@
+import { format } from "date-fns";
+import { ChevronUp } from "lucide-react";
 import Image from "next/image";
 import { RefObject } from "react";
 
 export default function SessionSection({
   ref,
   scrollToView,
+  groomName,
+  brideName,
+  place,
+  address,
+  resepsiTime,
 }: {
   ref: RefObject<HTMLDivElement | null>;
   scrollToView: (view: "view1" | "view2" | "view3") => void;
+  groomName?: string;
+  brideName?: string;
+  place?: string;
+  address?: string;
+  resepsiTime?: Date;
 }) {
   return (
     <section
@@ -27,33 +39,44 @@ export default function SessionSection({
           </h1>
 
           <p className='font-parisienne text-6xl font-bold text-center'>
-            Peter <br />& <br />
-            Siti
+            {groomName} <br />& <br />
+            {brideName}
           </p>
 
           <div className='flex flex-col items-center gap-4'>
-            <h3 className='text-2xl font-semibold'>MARCH</h3>
+            <h3 className='text-2xl font-semibold'>
+              {resepsiTime ? format(resepsiTime, "MMMM") : "-"}
+            </h3>
 
             <div className='w-full flex gap-8 justify-center items-center'>
               <p className='min-w-40 text-2xl text-center font-semibold border-b-2 border-t-2 py-1.5 border-black'>
-                SATURDAY
+                {resepsiTime ? format(resepsiTime, "EEEE") : "-"}
               </p>
-              <p className='text-6xl font-bold'>31</p>
+              <p className='text-6xl font-bold'>{resepsiTime?.getDate()}</p>
               <p className='min-w-40 text-2xl text-center font-semibold border-b-2 border-t-2 py-1.5 border-black'>
-                AT 08:00 PM
+                AT {resepsiTime ? format(resepsiTime, "hh:mm aa") : "-"}
               </p>
             </div>
-            <p className='text-2xl font-semibold'>2026</p>
+            <p className='text-2xl font-semibold'>
+              {resepsiTime?.getFullYear()}
+            </p>
 
             <div className='text-center mt-8'>
-              <p className='text-2xl font-bold'>Graha Wangsa Ballroom</p>
+              <p className='text-2xl font-bold'>{place}</p>
 
-              <p className='text-lg'>
-                Jalan Sultan Iskandar Muda, Jakarta Selatan
-              </p>
+              <p className='text-lg'>{address}</p>
             </div>
           </div>
         </div>
+
+        <button
+          className='absolute bottom-8 left-1/2 -translate-x-1/2 animate-bounce'
+          onClick={() => scrollToView("view1")}>
+          <ChevronUp
+            size={32}
+            className='text-black'
+          />
+        </button>
       </div>
     </section>
   );
