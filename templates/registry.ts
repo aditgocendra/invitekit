572aac12f811e9dc8e069526f7b9f68f74ea9403
aditgocendra <gocendra123@gmail.com>
2026-02-01 22:03:20 +0700

    add FormComponent in registry schema

--- templates/registry.ts
+++ templates/registry.ts
@@ -1,10 +1,12 @@
 import FreeWeddingMinimalTemplate from "./free/wedding/minimal";
+import FormWeddingMinimal from "./free/wedding/minimal/component/form-w-minimal";
 import { freeWeddingMinimalConfig } from "./free/wedding/minimal/config";
 
 export const TEMPLATE_REGISTRY = {
   [freeWeddingMinimalConfig.key]: {
     ...freeWeddingMinimalConfig,
     Component: FreeWeddingMinimalTemplate,
+    FormComponent: FormWeddingMinimal,
   },
   // template lain...
 } as const;
