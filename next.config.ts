bcd67baccc5f64964eeac0d8eee77daa9d3acc52
aditgocendra <gocendra123@gmail.com>
2026-02-01 21:55:45 +0700

    add remote

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,19 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "s3.nevaobjects.id",
+      },
+
+      {
+        protocol: "https",
+        hostname: "s3.nevaobjects.id",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
