0263e9ca1378065afae6cc2d9bb8e1d518863f3a
aditgocendra <gocendra123@gmail.com>
2026-02-17 20:31:12 +0700

    add new template (basic)

--- templates/registry.ts
+++ templates/registry.ts
@@ -1,3 +1,6 @@
+import BasicWeddingMinimalTemplate from "./basic/wedding/minimal";
+import FormWeddingBasicMinimal from "./basic/wedding/minimal/component/form-bw-minimal";
+import { basicWeddingMinimalConfig } from "./basic/wedding/minimal/config";
 import FreeWeddingMinimalTemplate from "./free/wedding/minimal";
 import FormWeddingMinimal from "./free/wedding/minimal/component/form-w-minimal";
 import { freeWeddingMinimalConfig } from "./free/wedding/minimal/config";
@@ -7,6 +10,11 @@ export const TEMPLATE_REGISTRY = {
     ...freeWeddingMinimalConfig,
     Component: FreeWeddingMinimalTemplate,
     FormComponent: FormWeddingMinimal,
+  },
+  [basicWeddingMinimalConfig.key]: {
+    ...basicWeddingMinimalConfig,
+    Component: BasicWeddingMinimalTemplate,
+    FormComponent: FormWeddingBasicMinimal,
   },
   // template lain...
 } as const;
