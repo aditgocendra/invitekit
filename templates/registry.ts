92f69396af43af4f01d5a3cdde1e9a0672217a5f
aditgocendra <gocendra123@gmail.com>
2026-03-05 20:41:47 +0700

    add new template : pw-film

--- templates/registry.ts
+++ templates/registry.ts
@@ -4,6 +4,9 @@ import { basicWeddingMinimalConfig } from "./basic/wedding/minimal/config";
 import FreeWeddingMinimalTemplate from "./free/wedding/minimal";
 import FormWeddingMinimal from "./free/wedding/minimal/component/form-w-minimal";
 import { freeWeddingMinimalConfig } from "./free/wedding/minimal/config";
+import PremiumWeddingFilmTemplate from "./premium/wedding/film";
+import FormWeddingPremiumFilm from "./premium/wedding/film/component/form-pw-film";
+import { premiumWeddingFilmConfig } from "./premium/wedding/film/config";
 
 export const TEMPLATE_REGISTRY = {
   [freeWeddingMinimalConfig.key]: {
@@ -15,6 +18,11 @@ export const TEMPLATE_REGISTRY = {
     ...basicWeddingMinimalConfig,
     Component: BasicWeddingMinimalTemplate,
     FormComponent: FormWeddingBasicMinimal,
+  },
+  [premiumWeddingFilmConfig.key]: {
+    ...premiumWeddingFilmConfig,
+    Component: PremiumWeddingFilmTemplate,
+    FormComponent: FormWeddingPremiumFilm,
   },
   // template lain...
 } as const;
