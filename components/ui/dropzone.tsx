d4c2def98aa9c7999577824d29d88f915cfb2f03
aditgocendra <gocendra123@gmail.com>
2026-03-18 20:42:57 +0700

    update:support audio file

--- components/ui/dropzone.tsx
+++ components/ui/dropzone.tsx
@@ -1,13 +1,14 @@
 import ReactDropzone, { DropzoneOptions } from "react-dropzone";
 import { Input } from "./input";
 import { cn } from "@/lib/utils";
-import { ImageDown } from "lucide-react";
+import { AudioLines, ImageDown } from "lucide-react";
 
 interface DropzoneProps extends DropzoneOptions {
   className?: string;
+  fileType?: "image" | "audio";
 }
 
-const ACCEPTED_FILES = {
+const ACCEPTED_IMAGES = {
   "image/*": [
     ".jpg",
     ".jpeg",
@@ -21,24 +22,45 @@ const ACCEPTED_FILES = {
   ],
 };
 
-export default function Dropzone({ className, ...props }: DropzoneProps) {
+const ACCEPTED_AUDIO = {
+  "audio/*": [".mp3", ".wav", ".ogg", ".m4a", ".aac", ".flac"],
+};
+
+export default function Dropzone({
+  className,
+  fileType = "image",
+  ...props
+}: DropzoneProps) {
+  if (fileType === "image") {
+    props.accept = ACCEPTED_IMAGES;
+  } else if (fileType === "audio") {
+    props.accept = ACCEPTED_AUDIO;
+  }
+
   return (
     <ReactDropzone
-      accept={ACCEPTED_FILES}
+      // accept={ACCEPTED_FILES}
       {...props}>
       {({ getRootProps, getInputProps }) => (
         <div
           {...getRootProps()}
           className={cn(
             "bg-gray-50 rounded-md shadow-sm border-2 border-dashed cursor-pointer flex items-center justify-center",
-            className
+            className,
           )}>
           <Input {...getInputProps()} />
           <div>
-            <ImageDown
-              size={32}
-              className='text-gray-500'
-            />
+            {fileType === "image" ? (
+              <ImageDown
+                size={32}
+                className='text-gray-500'
+              />
+            ) : (
+              <AudioLines
+                size={32}
+                className='text-gray-500'
+              />
+            )}
           </div>
         </div>
       )}
