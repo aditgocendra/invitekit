2dfd3225e2cf84a6727666b189fc5d933235d229
aditgocendra <gocendra123@gmail.com>
2026-04-01 22:28:43 +0700

    add array object

--- validation/event.validation.ts
+++ validation/event.validation.ts
@@ -1,6 +1,12 @@
 import z from "zod";
 
 export const InvitationEventFormSchema = z.object({
-  name: z.string().min(1, "Name is required"),
-  phoneNumber: z.string().min(1, "Phone number is required").max(14),
+  guests: z
+    .array(
+      z.object({
+        name: z.string().min(1, "Name is required"),
+        phoneNumber: z.string().min(1, "Phone number is required").max(14),
+      }),
+    )
+    .min(1), // Optional: enforce at least one guest
 });
