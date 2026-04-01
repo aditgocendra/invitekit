6f772a8b0aea897990d50673b522f1ca50a77cdb
aditgocendra <gocendra123@gmail.com>
2026-04-01 22:29:11 +0700

    update : multiple guest invite

--- components/form/form-invitation.tsx
+++ components/form/form-invitation.tsx
@@ -2,8 +2,9 @@ import z from "zod";
 import { Button } from "../ui/button";
 import { FieldInput } from "../ui/field";
 import { InvitationEventFormSchema } from "@/validation/event.validation";
-import { Resolver, useForm } from "react-hook-form";
+import { Resolver, useFieldArray, useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
+import { Trash2 } from "lucide-react";
 
 type InvitationEventInput = z.infer<typeof InvitationEventFormSchema>;
 
@@ -16,7 +17,25 @@ export default function FormInvitation({ onSubmit }: Props) {
     resolver: zodResolver(
       InvitationEventFormSchema,
     ) as Resolver<InvitationEventInput>,
+    defaultValues: {
+      guests: [
+        {
+          name: "",
+          phoneNumber: "",
+        },
+      ],
+    },
   });
+
+  const { fields, append, remove } = useFieldArray({
+    control: form.control,
+    name: "guests", // Matches schema
+  });
+
+  const addMoreField = () => {
+    if (fields.length >= 10) return;
+    append({ name: "", phoneNumber: "" });
+  };
 
   const onSubmitHandler = (data: InvitationEventInput) => {
     onSubmit(data);
@@ -26,21 +45,51 @@ export default function FormInvitation({ onSubmit }: Props) {
     <form
       onSubmit={form.handleSubmit(onSubmitHandler)}
       className='space-y-4'>
-      <FieldInput
-        label='Guest'
-        placeholder='Ex : Yono & Yini'
-        {...form.register("name")}
-      />
+      {fields.map((field, index) => (
+        <div
+          key={field.id}
+          className='flex gap-2 items-end border rounded-lg p-2'>
+          <FieldInput
+            label='Guest'
+            placeholder='Ex : Yono & Yini'
+            {...form.register(`guests.${index}.name`)}
+            errorMessage={form.formState.errors.guests?.[index]?.name?.message}
+          />
+          <FieldInput
+            label='Phone Number (Whatsapp)'
+            type='number'
+            placeholder='Ex : 628xxxxxxxxx'
+            {...form.register(`guests.${index}.phoneNumber`, {
+              required: "Phone number is required",
+              maxLength: 14,
+            })}
+            errorMessage={
+              form.formState.errors.guests?.[index]?.phoneNumber?.message
+            }
+          />
+          <Button
+            type='button'
+            variant={"outline"}
+            size={"icon"}
+            onClick={() => {
+              if (fields.length > 1) {
+                remove(index);
+              }
+            }}>
+            <Trash2 />
+          </Button>
+        </div>
+      ))}
 
-      <FieldInput
-        label='Phone Number (Whatsapp)'
-        type='number'
-        placeholder='Ex : 628xxxxxxxxx'
-        {...form.register("phoneNumber")}
-      />
-
-      <div className='flex justify-end items-end'>
-        <Button type='submit'>Invite</Button>
+      <div className='flex justify-end items-end gap-2'>
+        <Button
+          type='button'
+          variant={"outline"}
+          disabled={fields.length >= 10}
+          onClick={addMoreField}>
+          Add More Guest
+        </Button>
+        <Button type='submit'>Send Invitation</Button>
       </div>
     </form>
   );
